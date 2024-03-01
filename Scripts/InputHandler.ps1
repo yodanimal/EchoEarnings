11fbf651957ac27a127a864ee4ace094c3d134d8
Rick Evans <rick.evans@ballista.com>
2024-03-01 17:41:26 -0500

    use getter to get values

--- Scripts/InputHandler.ps1
+++ Scripts/InputHandler.ps1
@@ -6,15 +6,23 @@ enum PromptDataType {
     Decimal = 1
 }
 
-$HourlyWage = 'H'
-$SalaryWage = 'S'
+function Get-HourlyWageValue() {
+    [OutputType([string])]
+    $Value = 'H'
+    return $Value
+}
 
+function Get-SalaryWageValue() {
+    [OutputType([string])]
+    $Value = 'S'
+    return $Value
+}
 function Get-UserInput {
-    $prompt = "Enter **$($HourlyWage)** for hourly wage or **$($SalaryWage)** for salary"
+    $prompt = "Enter **$(Get-HourlyWageValue)** for hourly wage or **$(Get-SalaryWageValue)** for salary"
     $inputType = Get-ValidatedInput $prompt ([PromptDataType]::Choice)
-    if ($inputType -eq $HourlyWage) {
+    if ($inputType -eq (Get-HourlyWageValue)) {
         $value = Get-ValidatedInput "Enter your hourly wage" ([PromptDataType]::Decimal)
-    } elseif ($inputType -eq $SalaryWage) {
+    } elseif ($inputType -eq (Get-SalaryWageValue)) {
         $value = Get-ValidatedInput "Enter your annual salary" ([PromptDataType]::Decimal)
     }
     return $inputType, $value
@@ -32,7 +40,7 @@ function Get-ValidatedInput {
                 [decimal]$validatedInput = $userInput
                 return $validatedInput
             } elseif ($type -eq [PromptDataType]::Choice) {
-                if ($userInput -ne $HourlyWage -and $userInput -ne $SalaryWage) { throw }
+                if ($userInput -ne (Get-HourlyWageValue) -and $userInput -ne (Get-SalaryWageValue)) { throw }
                 return $userInput
             }
         } catch {
